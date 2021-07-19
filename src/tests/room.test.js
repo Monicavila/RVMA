763aa568d54ccc7ca70f6a2a4eeaefe3a9a205a2
Monicavila <di.monicavila@gmail.com>
2021-07-18 22:58:38 -0500

    RMVA-23 snapshot room done

--- src/tests/room.test.js
+++ src/tests/room.test.js
@@ -3,14 +3,6 @@ import renderer from 'react-test-renderer';
 import { act } from "react-dom/test-utils";
 import Room from '../components/room/room';
 
-test('renders learn react link', () => {
-    act(()=>{
-      render(<Room />);
-    })
-    const linkElement = screen.getByText("OK!");
-    expect(linkElement).toBeInTheDocument();
-  });
-
 it("renders the Landing snapshot", () => {
     const tree = renderer.create(<Room />).toJSON();
     expect(tree).toMatchSnapshot();
