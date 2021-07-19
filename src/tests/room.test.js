139e36ab38bb8af62935bc6b21200c745fab2b1c
Monicavila <di.monicavila@gmail.com>
2021-07-19 16:18:08 -0500

    RVMA-23 room snapshot update

--- src/tests/room.test.js
+++ src/tests/room.test.js
@@ -1,6 +1,4 @@
-import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
-import { act } from "react-dom/test-utils";
 import Room from '../components/room/room';
 
 it("renders the Landing snapshot", () => {
